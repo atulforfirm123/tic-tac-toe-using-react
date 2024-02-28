122799ebf43382c217274072b66242f1492d653b
atulforfirm123 <atulkumargajar.forfirm@gmail.com>
2024-02-28 08:22:22 +0530

    tic tac toe using react

---
+++ tic tac toe/src/App.jsx
@@ -0,0 +1,13 @@
+import Board from './tic tac toe/Board';
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tic tac toe/src/tic tac toe/Square.jsx
@@ -0,0 +1,8 @@
+const Square=(props)=>{
+  return (
+    <div onClick={props.onClick} style={{border:'1px solid black',width:'100%',height:'100px',display:'flex',justifyContent:'center',alignItems:'center'}} className="square">
+      <h5>{props.value}</h5>
+    </div>
+  )
+}
+export default Square;
